53fc3067a37543782abe8fcee7aeaee61dac881e
unknown <you>
2020-01-08 21:05:22 +0800

    开始准备完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入rem 适配
+import 'amfe-flexible'
+// 引入vant模块
+import './utils/model-vant'
+// 引入样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
