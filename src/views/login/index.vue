306ee9053d0c5c233e9473d0ff93290510090c6f
unknown <you>
2020-01-08 22:28:46 +0800

    登录基本布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,21 @@
 <template>
-<div>aaa</div>
+  <div class="login-container">
+    <!-- 导航 -->
+    <van-nav-bar title="标题" />
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field required clearable label="手机号" placeholder="请输入手机号" />
+      <van-field label="验证码" placeholder="请输入验证码" required />
+    </van-cell-group>
+    <div>
+      <van-button type="info">信息按钮</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' >
 </style>
