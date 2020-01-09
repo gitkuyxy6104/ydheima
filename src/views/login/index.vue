a97acce773687638b9c3080e65f41e478f57f2f4
unknown <you>
2020-01-09 16:17:00 +0800

    发送验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,12 +14,16 @@
           <i class="icon icon-yanzhengma" style="font-size:20px;padding-right:5px"></i>
         </template>
         <van-button
+          v-if="!isCountDownShow"
          slot="button"
          size="small"
          type="primary"
-         round>发送验证码
+         round
+         @click="onSendSmsCode"
+         >发送验证码
         </van-button>
         <van-count-down
+          v-else
          :time="60 * 1000"
          slot="button"
          format="ss 秒"
@@ -33,17 +37,34 @@
 </template>
 
 <script>
-import { login } from '@/api/user'
+import { login, getSmsCode } from '@/api/user'
 export default {
   data () {
     return {
       user: {
         mobile: '', // 手机号
         code: '' // 验证码
-      }
+      },
+      isCountDownShow: false // 是否显示倒计时
     }
   },
   methods: {
+    // 获取验证码
+    async onSendSmsCode () {
+      try {
+        const { mobile } = this.user
+        // 检验手机号是否有效
+        // 请求发短信
+        const res = await getSmsCode(mobile)
+        console.log(res)
+        // 显示时间
+        this.isCountDownShow = true
+      } catch (err) {
+        console.log(err)
+        this.$toast('请勿频繁操作')
+      }
+    },
+    // 登录请求
     async onLogin () {
       // 获取数据
       const user = this.user
