e954a996b729c4e8e3811f158f991eb2a2c116ff
unknown <you>
2020-01-12 18:02:07 +0800

    首页文章列表-封装文章列表组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,23 +10,48 @@
       title 标签标题
      -->
     <van-tabs v-model="active">
-      <van-tab title="标签 1">
+      <van-tab
+        :title="channel.name"
+        v-for="channel in userChannels"
+        :key="channel.id"
+      >
         <!-- 文章列表 -->
+        <article-list :channel="channel" />
         <!-- /文章列表 -->
       </van-tab>
-      <van-tab title="标签 2">内容 2</van-tab>
+      <!-- <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab> -->
     </van-tabs>
     <!-- /频道列表 -->
   </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/channel'
+import ArticleList from './components/article-list'
 export default {
   data () {
     return {
-      active: 2
+      active: 0, // 控制激活的标签页
+      userChannels: [] // 用户频道列表
+    }
+  },
+  components: {
+    ArticleList
+  },
+  created () {
+    this.loadUserChannels()
+  },
+  methods: {
+    async loadUserChannels () {
+      try {
+        const { data } = await getUserChannels()
+        this.userChannels = data.data.channels
+      } catch (err) {
+        console.log(err)
+        this.$toast('获取频道数据失败')
+      }
     }
   }
 }
